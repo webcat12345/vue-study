595bdfda82434dc5819399e1de14754ecf58ed48
webcat12345 <webcat91@gmail.com>
2019-12-25 14:38:13 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" title="This component is First vue component"/>
+    <Sidebar v-bind:status="true"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Sidebar from './components/Sidebar';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Sidebar
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+    <p v-show="status">Component Testing: {{ status }}</p>
+</template>
+
+<script>
+    export default {
+        name: 'Sidebar',
+        props: {
+            status: Boolean
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
